547e5201f9692419b588ebbff2aabadb6f0ad2b1
Phil Bauer <pjbauer@ec.rr.com>
2015-11-13 18:26:18 -0500

    Update fabrik-element-databasejoin-form-description.php

--- plugins/fabrik_element/databasejoin/layouts/fabrik-element-databasejoin-form-description.php
+++ plugins/fabrik_element/databasejoin/layouts/fabrik-element-databasejoin-form-description.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Cascading drop down front end select layout
+ * Database join and Cascading drop down front end select layout
  */
 defined('JPATH_BASE') or die;
 
 $d = $displayData;
-
 ?>
 <div class="dbjoin-description">
 	<?php
 	// @FIXME - if read only, surely no need to insert every possible value, we just need the selected one?
 	for ($i = 0; $i < count($d->opts); $i++) :
 		$opt = $d->opts[$i];
-		$display = $opt->value == $d->default ? '' : 'style="display: none"';
-		//$c = $d->showPleaseSelect ? $i + 1 : $i;
+		$display = $opt->value == $d->defaultRaw ? '' : ' style="display: none"';
+		// phil changed - this will mess up list - 
+		//$c = $d->showPleaseSelect ? $i + 1 : $i;		
+		// also, pk value now used instead of i as part of id an identifier (instead of class)  		
 	?>
-		<div <?php echo $display ;?> class="notice description-<?php echo  $i; ?>">
+		<div class="notice" id="<?php echo 'dbjdesc-'.$d->fullElName.'-'.$opt->value;?>"<?php echo $display;?>>
 			<?php echo $opt->description; ?>
 		</div>
 	<?php
