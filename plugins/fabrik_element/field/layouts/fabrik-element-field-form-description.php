58a2ad24fb3622482bd40f7489a90b3c19e18a11
Phil Bauer <pjbauer@ec.rr.com>
2015-11-13 17:40:31 -0500

    Update fabrik-element-field-form-description.php

--- plugins/fabrik_element/field/layouts/fabrik-element-field-form-description.php
+++ plugins/fabrik_element/field/layouts/fabrik-element-field-form-description.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Cascading drop down front end select layout
+ * Generic global element description layout
  */
 defined('JPATH_BASE') or die;
 
 $d = $displayData;
-//var_dump($d);
+
 ?>
 <div class="<?php echo $d->opts['elname'];?>___description">
 	<?php echo $d->opts['description']; ?>
